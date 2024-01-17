67cad7160079557c9ffcb71294c328a25784f4bf
Leandro_Vitor <leandrovitor455@gmail.com>
2024-01-16 22:29:56 -0300

    Base64 description done

--- main.py
+++ main.py
@@ -92,14 +92,34 @@ def hlp():
 
             result Fnnc axd
           
-        A1Z26
+        A1Z26(text)
           Recive text(string) and replace letters to numbers based on their place index. Example:
 
           text    I love you
 
           result  09 12152205 251521
 
-        Base64 ()
+        Base64(ascii letters)
+          receive an input(everything on ascii table) and makes some kind of conversion,
+          first the string is converted to binary where every character is 8 bits or one byte
+
+          01000010 01111001 01100101
+              B        y       e
+          
+          then the binary string is splited 3 bytes each just like the example above, and anothe
+          spit it's made for each 6 bits
+ 
+          010000 100111 100101 100101
+             Q     n      l      l
+          
+          and the 4 binary sets are converted to numbers that are indexes on Base64 "alphabet" that go
+          from a to z lower and upper, 0 to 9, "+" and "/"
+
+          010010 000110 1001  None
+             S     G      k     =
+          
+          when 3 bytes can't be formed a equal sign "=" is put in place.
+
         Binary
         Hexadecimal
           
